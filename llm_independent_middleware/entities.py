90f545771c53988a7e01974a46718470091feacb
Gebelubo <gabrielluiz2004@gmail.com>
2024-10-14 22:23:42 -0300

    first refactorings

--- llm_independent_middleware/entities.py
+++ llm_independent_middleware/entities.py
@@ -19,7 +19,7 @@ class Treatmentinput:
     
 
     # Passing here the name of the desired model if it is needed
-    model_name : Optional[str]
+    #model_name : Optional[str]
 
     #not defined yet
 
@@ -33,7 +33,7 @@ class Treatmentinput:
 class PromptValidation:
 
     # A exclusive identifier to the PromptValidation
-    prompt_validation_id : int
+    #prompt_validation_id : int
 
     # The name of the PromptValidation
     name : str
@@ -47,15 +47,15 @@ class PromptValidationCenter:
 
     # A list where all PromptValidations are going to be registered
     PromptValidations : list[PromptValidation] = [
-            PromptValidation(prompt_validation_id=1, name='length_validation', description='checks if the answer size is greater than 0', operation=len_test),
-            PromptValidation(prompt_validation_id=2, name='key_equal_value_validation', description='checks if the answer is equal to the key', operation=key_test),
-            PromptValidation(prompt_validation_id=3, name='and_validation', description='checks if the answer contains an addition mark', operation=and_test),
-            PromptValidation(prompt_validation_id=4, name='att_validation', description='checks if the answer is equal to some processed key', operation=att_test),
-            PromptValidation(prompt_validation_id=5, name='pronoun_validation', description='checks if there is an addition mark following a pronoun', operation=pronoun_test),
-            PromptValidation(prompt_validation_id=6, name='entity_validation', description='checks if the answer is equal to the entity found', operation=entity_test),
-            PromptValidation(prompt_validation_id=7, name='ignoring_validation', description='checks if the answer is ignoring some important information', operation=ignoring_test),
-            PromptValidation(prompt_validation_id=8, name='float_validation', description='checks if the wanted value is a float value, and then discover if the model correctly find the value or only the integer part', operation=float_test),
-            PromptValidation(prompt_validation_id=9, name='char_validation', description='checks if the answer has some noise characters', operation=char_test)
+            PromptValidation(name='length_validation', description='checks if the answer size is greater than 0', operation=len_test),
+            PromptValidation(name='key_equal_value_validation', description='checks if the answer is equal to the key', operation=key_test),
+            PromptValidation(name='and_validation', description='checks if the answer contains an addition mark', operation=and_test),
+            PromptValidation(name='att_validation', description='checks if the answer is equal to some processed key', operation=att_test),
+            PromptValidation(name='pronoun_validation', description='checks if there is an addition mark following a pronoun', operation=pronoun_test),
+            PromptValidation(name='entity_validation', description='checks if the answer is equal to the entity found', operation=entity_test),
+            PromptValidation(name='ignoring_validation', description='checks if the answer is ignoring some important information', operation=ignoring_test),
+            PromptValidation(name='float_validation', description='checks if the wanted value is a float value, and then discover if the model correctly find the value or only the integer part', operation=float_test),
+            PromptValidation(name='char_validation', description='checks if the answer has some noise characters', operation=char_test)
     ]
 
 
@@ -70,7 +70,7 @@ class PromptValidationCenter:
 class Treatment:
 
     # A exclusive identifier to the treatment
-    treatment_id : int
+    #treatment_id : int
 
     # The name of the treatment
     name : str
@@ -84,12 +84,12 @@ class TreatmentCenter:
 
     # A list where all treatments are going to be registered
     treatments : list[Treatment] = [
-        Treatment(treatment_id=2, name='new_request_treatment', description='A flag to indicate to the system to execute the treatments', operation=request_new_answer)
+        Treatment(name='new_request_treatment', description='A flag to indicate to the system to execute the treatments', operation=request_new_answer)
     ]
 
     # List of treatments that are made every tive before the regular ones
     mandatory_treatments : list[Treatment] = [
-        Treatment(treatment_id=1, name='similarity_filter', description='This treatment gets the interception between the response from the model and the user input', operation=similarity_filter)
+        Treatment(name='similarity_filter', description='This treatment gets the interception between the response from the model and the user input', operation=similarity_filter)
     ]
 
     """ 
@@ -108,7 +108,7 @@ class TreatmentCenter:
         }   
     """
     treatment_lines : dict[str, tuple[ list[Treatment], list[PromptValidation]  ]]
-
+    '''
     @classmethod
     def get_treatment_by_id(cls, id : int):
 
@@ -119,7 +119,7 @@ class TreatmentCenter:
             if tr.treatment_id == id:
                 return tr
         return None
-    
+    '''
     @classmethod
     def run_validations(cls, input : Treatmentinput, validations : list):
 
@@ -142,47 +142,26 @@ class TreatmentCenter:
     @classmethod
     def run_line(cls, line_name : str, input : Treatmentinput):
 
-        storage = ArangoQAModelStorage()
-
         treatments, validations = cls.treatment_lines[line_name]
         
         # executing mandatory treatments
         input = cls.run_mandatory_treatments(input)
 
-        model = storage.get_model(model['name'])
-        
-        # executing regular treatments and validations for each model
-        for model in storage.list_models():
+        # Making a deepcopy just to be sure that any treatment made with model A
+        # is passed to model B input 
+        input_ = deepcopy(input)
 
-            # Making a deepcopy just to be sure that any treatment made with model A
-            # is passed to model B input 
-            input_ = deepcopy(input)
-            input_.model_name = model['name']
+        # Adding None at the end of the treatments so it repeat one more that
+        # to validate the last treatment changes
+        for treatment in treatments + [None]:
 
-            # Adding None at the end of the treatments so it repeat one more that
-            # to validate the last treatment changes
-            for treatment in treatments + [None]:
+            if input_:
 
-                model_class = storage.get_model(model['name'])
-
-                for uid in list(map(lambda x: x['prompt_alternatives_uid'], filter(lambda x: 'prompt_alternatives_uid' in x, model_class.list_prompts()))):
-
-                    if input_:
-
-                        if cls.run_validations(input=input_, validations=validations):
-                            return input_
-                        
-                        elif treatment is not None:
-                            if treatment.name == 'new_request_treatment':
-                                input.prompt_id = uid
-                                input_ = treatment.operation(input) 
-                                input_ = cls.run_mandatory_treatments(input_) # executing mandatory treatments for the new input.
-                                break
-                            input_ = treatment.operation(input) 
-                            input_ = cls.run_mandatory_treatments(input_) # executing mandatory treatments for the new input.
-                            break
-
-        
+                if cls.run_validations(input=input_, validations=validations):
+                    return input_
+                
+                input_ = treatment.operation(input) 
+                input_ = cls.run_mandatory_treatments(input_) # executing mandatory treatments for the new input.
 
         # Just returned it because dont really know what to do when nothing goes right 
         return input
